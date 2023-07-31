e86e13240bf253ed5cb52b7e53566e43a34be690
octaviolan <octavio.olan25@gmail.com>
2023-07-31 10:05:06 -0600

    Responsive para laptop

---
+++ js/index.js
@@ -0,0 +1,13 @@
+console.log("enlazado");
+//Accediento a los input tipo number
+const bill = document.getElementById('bill');
+const tip = document.getElementById('tip');
+const people = document.getElementById('people');
+//Accedientos a los botones de porcentaje
+const percent = document.querySelectorAll('.calculator-input__percent button');
+//Acceder a los elementos de salida
+const amount = document.querySelector('.calculator-output__amount output')
+const total = document.querySelector('.calculator-output__total output')
+//Acceder al boton reiniciar
+const reset = document.getElementById('reset');
+
