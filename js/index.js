bcf8f1e0f4423141c26c9d7e65d1dffca7b5757a
octaviolan <octavio.olan25@gmail.com>
2023-07-31 12:38:20 -0600

    Refactorizar (DRY)

--- js/index.js
+++ js/index.js
@@ -14,6 +14,8 @@ const reset = document.getElementById('reset');
 //Regex: verificar input person
 const pattern = /^([1-9])([0-9]?)+$/;
 
+
+//Funcionalidad del area de botones para calcular por porcentaje
 percents.forEach((percent) => {
   percent.addEventListener('click', (e) => {
     if(!pattern.test(people.value)) {
@@ -22,32 +24,34 @@ percents.forEach((percent) => {
         error.classList.remove('isVisible');
       }, 3000)
     } else {
-      const percentValue = e.target.value/100;
-      const billValue = parseInt(bill.value);
-      const peopleValue = parseInt(people.value);
-      const tipAmount =  ((percentValue * billValue) / peopleValue).toFixed(2);
-      const totalPerson = (Number(tipAmount) + (billValue / peopleValue)).toFixed(2);
-      amount.textContent = `$${tipAmount}`;
-      total.textContent = `$${totalPerson}`;
+      calculator(e);
     }
   })
 })
 
+//Personalizar porcentaje
 custom.addEventListener('input', (e)=> {
   if(!pattern.test(e.target.value)) {
       return
   } else {
-      const percentValue = e.target.value/100;
-      const billValue = parseInt(bill.value);
-      const peopleValue = parseInt(people.value);
-      const tipAmount =  ((percentValue * billValue) / peopleValue).toFixed(2);
-      const totalPerson = (Number(tipAmount) + (billValue / peopleValue)).toFixed(2);
-      amount.textContent = `$${tipAmount}`;
-      total.textContent = `$${totalPerson}`;
+     calculator(e);
   }
 
 })
 
+//Funcion que calcula porcentaje
+function calculator(e) {
+  const percentValue = e.target.value/100;
+  const billValue = parseInt(bill.value);
+  const peopleValue = parseInt(people.value);
+  const tipAmount =  ((percentValue * billValue) / peopleValue).toFixed(2);
+  const totalPerson = (Number(tipAmount) + (billValue / peopleValue)).toFixed(2);
+  amount.textContent = `$${tipAmount}`;
+  total.textContent = `$${totalPerson}`;
+}
+
+
+//Resetear los valores originales
 reset.addEventListener('click', ()=> {
   bill.value = "";
   people.value = "";
