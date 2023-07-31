02e7b4cc90b1de220d098e0e9910efa006bc9bca
octaviolan <octavio.olan25@gmail.com>
2023-07-31 12:32:24 -0600

    Imput custom javascript

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 //Accediento a los input tipo number
 const bill = document.getElementById('bill');
-const tip = document.getElementById('tip');
+const custom = document.getElementById('tip');
 const people = document.getElementById('people');
 //Accedientos a los botones de porcentaje
 const percents = document.querySelectorAll('.calculator-input__percent button');
@@ -13,7 +13,6 @@ const total = document.querySelector('.calculator-output__total output')
 const reset = document.getElementById('reset');
 //Regex: verificar input person
 const pattern = /^([1-9])([0-9]?)+$/;
-
 
 percents.forEach((percent) => {
   percent.addEventListener('click', (e) => {
@@ -34,9 +33,25 @@ percents.forEach((percent) => {
   })
 })
 
+custom.addEventListener('input', (e)=> {
+  if(!pattern.test(e.target.value)) {
+      return
+  } else {
+      const percentValue = e.target.value/100;
+      const billValue = parseInt(bill.value);
+      const peopleValue = parseInt(people.value);
+      const tipAmount =  ((percentValue * billValue) / peopleValue).toFixed(2);
+      const totalPerson = (Number(tipAmount) + (billValue / peopleValue)).toFixed(2);
+      amount.textContent = `$${tipAmount}`;
+      total.textContent = `$${totalPerson}`;
+  }
+
+})
+
 reset.addEventListener('click', ()=> {
-  bill.value = 0;
-  people.value = 0;
+  bill.value = "";
+  people.value = "";
+  custom.value = "";
   amount.textContent = "$0.00";
   total.textContent = "$0.00";
 })
